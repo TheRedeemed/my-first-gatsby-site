eddb7ada8b9fd5b9cacb25e1a9af7dd801d5ab48
TheRedeemed <abdoulkaders77@gmail.com>
2022-06-20 00:51:20 -0400

    Adding app layout styled home and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../components/layout';
+
+const aboutPage = () => (
+    <Layout pageTitle="About Me">
+        <p>Hi There! I'm the proud cerator of this site which is built with Gatsby</p>
+    </Layout>
+)
+
+export default aboutPage;
