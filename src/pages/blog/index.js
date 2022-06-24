20f13e057b7b8311fae4b0eb6609c1ef15d8bb70
TheRedeemed <abdoulkaders77@gmail.com>
2022-06-24 00:08:33 -0400

    Finished Gatsby Tutorial Part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+import { Link, graphql } from "gatsby";
+
+const BlogPage = ({data}) => {
+    console.log(data);
+    return (
+        <Layout pageTitle="My Blog Posts">
+            <ul>
+                {
+                    data.allMdx.nodes.map(post => (
+                        <article key={post.id}>
+                            <h2>
+                                <Link to={`/blog/${post.slug}`}>
+                                    {post.frontmatter.title}
+                                </Link>
+                            </h2>
+                            <p>Posted: {post.frontmatter.date}</p>
+                        </article>
+                    ))
+                }
+            </ul>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+
+export default BlogPage
